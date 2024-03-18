d66cc565e37e19b823d5f767887962537b7d830c
Subhrajit Roy <81874861+SROY9697@users.noreply.github.com>
2024-03-18 16:24:46 +0530

    Update habit.js

--- routes/habit.js
+++ routes/habit.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const habitController = require('../controllers/habbit_controller')
+const habitController = require('../controllers/habit_controller')
 
 const router = express.Router();
 
