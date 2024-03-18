45daef760afe6a6d3338f00e862a21ad867121a9
Subhrajit Roy <81874861+SROY9697@users.noreply.github.com>
2024-03-18 15:51:35 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://sayansroy1997:qMwrIAXsZOorOMTl@cluster0.ohm8rms.mongodb.net/Habit"
+);
+// mongoose.connect('mongodb://localhost/habit-tracker');
+
+const db = mongoose.connection;
+
+//if error then pritnt message
+db.on("error", console.error.bind(console, "error in connectin DB"));
+
+// server is up then run a message
+db.once("open", () => {
+  console.log("Succesfully !! Connected to the DataBase");
+});
+module.export = db;
 
